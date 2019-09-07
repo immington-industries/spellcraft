cb81af913b96353e7a88d30083f2c5ac0f072853
S. B. Immington <snirk.immington@gmail.com>
2019-09-07 17:55:04 -0400

    Add undead scepter

--- src/main/java/industries/immington/spellcraft/Spellcraft.java
+++ src/main/java/industries/immington/spellcraft/Spellcraft.java
@@ -80,6 +80,8 @@ public class Spellcraft
             event.getRegistry().registerAll(
                 new ScepterItem(ItemTier.WOOD, 2, -2.2F, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:wooden_scepter"),
+                new ScepterItem(ItemTier.WOOD, 3, -2F, new Item.Properties().group(ItemGroup.TOOLS))
+                    .setRegistryName("spellcraft:undead_scepter"),
                 new ScepterItem(ItemTier.WOOD, 3, -2.1F, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:ocean_scepter"),
                 new ScepterItem(ItemTier.GOLD, 3, -2F, new Item.Properties().group(ItemGroup.TOOLS))
