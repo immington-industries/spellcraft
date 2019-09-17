921f3ab6dda171385893292862507a023bed6e64
S. B. Immington <snirk.immington@gmail.com>
2019-09-17 13:53:43 -0400

    Call spell methods from scepter

--- src/main/java/industries/immington/spellcraft/item/ScepterItem.java
+++ src/main/java/industries/immington/spellcraft/item/ScepterItem.java
@@ -1,52 +1,129 @@
 package industries.immington.spellcraft.item;
 
 import industries.immington.spellcraft.Spellcraft;
-import industries.immington.spellcraft.spell.ISpellItem;
+import industries.immington.spellcraft.Spells;
+import industries.immington.spellcraft.spell.PlayerSpellcaster;
+import industries.immington.spellcraft.spell.Spell;
+import industries.immington.spellcraft.spell.SpellDomain;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Base class for rods extends from sword.
  *
  * Keeps the sword's ability to break cobwebs.
  */
-public class ScepterItem extends SwordItem implements ISpellItem {
-
+public class ScepterItem extends SwordItem {
     private static final Logger LOGGER = LogManager.getLogger(Spellcraft.MOD_ID);
 
-    public ScepterItem(IItemTier tier, int attackDamage, float attackSpeed, Item.Properties propertiesBuilder) {
+    private static final String CASTING_TAG = "spellcraft:Casting";
+
+    private final SpellDomain spellDomain;
+
+    public ScepterItem(IItemTier tier, int attackDamage, float attackSpeed, @Nonnull SpellDomain domain, Item.Properties propertiesBuilder) {
         super(tier, attackDamage, attackSpeed, propertiesBuilder);
+        this.addPropertyOverride(new ResourceLocation("spellcraft:cast_time"), (stack, world, entity) -> {
+            // get stack's casting info
+            // calculate potency range
+            // negative for chargeup
+            return 0F;
+        });
+        this.spellDomain = domain;
+        this.addPropertyOverride(new ResourceLocation("spellcraft:is_casting"),
+                (stack, world, entity) ->
+                        entity != null && entity.isHandActive() && entity.getActiveItemStack() == stack ? 1F : 0F);
+    }
+
+    public Optional<SpellDomain> getDomainFocus() {
+        return Optional.of(this.spellDomain);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag tooltipFlag) {
+        super.addInformation(stack, world, tooltip, tooltipFlag);
+        Optional<SpellDomain> domain = this.getDomainFocus();
+        if (domain.isPresent()) {
+            tooltip.add(new TranslationTextComponent(this.spellDomain.getRegistryName().toString()));
+        }
+    }
+
+    public static boolean hasSpell(ItemStack stack) {
+        CompoundNBT spellTag = stack.getChildTag(CASTING_TAG);
+            return spellTag != null &&
+                    (spellTag.contains("Spell") || spellTag.contains("SelectedSpell"));
+    }
+
+    public static Optional<Spell> getSpell(ItemStack stack) {
+        CompoundNBT spellTag = stack.getChildTag(CASTING_TAG);
+        if (spellTag == null) { return Optional.empty(); }
+        if (spellTag.contains("Spell")) {
+            String spellId = spellTag.getString("Spell");
+            ResourceLocation spellLocation = ResourceLocation.tryCreate(spellId);
+            if (spellLocation != null) {
+                return Optional.ofNullable(Spells.registry.getValue(spellLocation));
+            }
+        }
+        else if (spellTag.contains("SelectedSpell")) {
+            String spellId = spellTag.getString("SelectedSpell");
+            ResourceLocation spellLocation = ResourceLocation.tryCreate(spellId);
+            if (spellLocation != null) {
+                return Optional.ofNullable(Spells.registry.getValue(spellLocation));
+            }
+        }
+        return Optional.empty();
     }
 
     @Override
     public int getMaxDamage(ItemStack stack) {
-        CompoundNBT spellTag = stack.getChildTag("spellcraft:casting");
-        if (spellTag != null && !spellTag.isEmpty()) {
-            return spellTag.getInt("max_damage");
+        if (ScepterItem.hasSpell(stack)) {
+            return stack.getChildTag(CASTING_TAG).getInt("MaxCasts");
         }
         return 0;
     }
 
+    /**
+     * One of each type of spellcasting item is added to the creative tabs.
+     * @param group
+     * @param items
+     */
+    @Override
+    public void fillItemGroup(@Nonnull ItemGroup group, @Nonnull NonNullList<ItemStack> items) {
+        if (group == ItemGroup.TOOLS || group == ItemGroup.SEARCH) {
+            items.add(new ItemStack(this));
+            for (Spell spell : Spells.registry.getValues()) {
+                if (!spell.getDomain().equals(this.spellDomain)) {
+                    continue;
+                }
+                ItemStack scepter = new ItemStack(this);
+                CompoundNBT castingTag = scepter.getOrCreateChildTag(CASTING_TAG);
+                castingTag.putString("Spell", spell.getRegistryName().toString());
+                items.add(scepter);
+            }
+        }
+    }
+
     @Override
     public boolean hasEffect(ItemStack stack) {
-        return true;
-        /* should check if it has a spellcraft:casting tag */
+        return ScepterItem.hasSpell(stack);
     }
 
     @Override
     public int getUseDuration(ItemStack stack) {
-        return 256;
+        return 72000;
         /*
         CompoundNBT spellTag = stack.getChildTag("spellcraft:casting");
         if (spellTag != null && !spellTag.isEmpty()) {
@@ -67,19 +144,42 @@ public class ScepterItem extends SwordItem implements ISpellItem {
 
     @Override
     @Nonnull
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BLOCK;
+    }
+
+    @Override
+    @Nonnull
     public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, @Nonnull Hand hand) {
         ItemStack thisItem = player.getHeldItem(hand);
-        ItemStack offhandItem = hand == Hand.MAIN_HAND ? player.getHeldItemOffhand() : player.getHeldItemMainhand();
+        player.setActiveHand(hand);
+        player.setHeldItem(hand, thisItem);
 
-        if (world.isRemote) {
-            world.addParticle(ParticleTypes.FLASH, player.posX, player.posY + player.getEyeHeight(), player.posZ, 0, 0, 0);
+        if (!world.isRemote) {
+            LOGGER.info("Server item right click, checking spell info.");
+        }
+        Optional<Spell> maybeSpell = ScepterItem.getSpell(thisItem);
+        if (maybeSpell.isPresent()) {
+            if (!world.isRemote) {
+                LOGGER.info("Server found spell {}, preparing to cast", maybeSpell.get());
+            }
+            Spell spell = maybeSpell.get();
+            if (!spell.checkCastingBegin(world, new PlayerSpellcaster(player))) {
+                if (!world.isRemote) {
+                    LOGGER.info("Cannot cast spell {}!", spell.getRegistryName());
+                }
+                return new ActionResult<>(ActionResultType.FAIL, thisItem);
+            }
+            if (maybeSpell.get().onWarmupTick(world, new PlayerSpellcaster(player), 0)) {
+                return new ActionResult<>(ActionResultType.SUCCESS, thisItem);
+            }
+            else {
+                return new ActionResult<>(ActionResultType.FAIL, thisItem);
+            }
         }
         else {
-            LOGGER.info("Begin casting with {}", thisItem);
+            return new ActionResult<>(ActionResultType.PASS, thisItem);
         }
-
-        player.setActiveHand(hand);
-        return new ActionResult<>(ActionResultType.SUCCESS, thisItem);
     }
 
     @Override
@@ -89,36 +189,69 @@ public class ScepterItem extends SwordItem implements ISpellItem {
     }
 
     @Override
-    public void onUsingTick(ItemStack stack, LivingEntity player, int count) {
-        if (count % 10 == 0) {
-            if (player.world.isRemote()) {
-                player.world.addParticle(ParticleTypes.EFFECT, player.posX, player.posY + player.getEyeHeight(), player.posZ, 0, 0, 0);
+    public void onUsingTick(ItemStack stack, LivingEntity player, int remaining) {
+        if (!(player instanceof PlayerEntity)) { return; }
+        if (remaining % 5 == 0 && !player.getEntityWorld().isRemote) {
+            LOGGER.info("Item is ticking");
+        }
+        Optional<Spell> storedSpell = ScepterItem.getSpell(stack);
+        if (storedSpell.isPresent()) {
+            Spell spell = storedSpell.get();
+            int ticks = this.getUseDuration(stack) - remaining;
+            int castingTicks = spell.getChargeTime() - ticks;
+            if (castingTicks > 0) {
+                if (!player.getEntityWorld().isRemote) {
+                    LOGGER.info("Ticking spell during cast {}", spell);
+                }
+                spell.onWarmupTick(player.getEntityWorld(), new PlayerSpellcaster((PlayerEntity) player), ticks);
             }
             else {
-                LOGGER.info("- casting tick {} ({})", count / 10, stack);
+                LOGGER.info("Ticking spell after cast {}", spell);
+                spell.onCastTick(player.getEntityWorld(), new PlayerSpellcaster((PlayerEntity) player), -ticks);
             }
         }
     }
+
+
 
     @Override
     public void onPlayerStoppedUsing(ItemStack stack, World world, LivingEntity entity, int timeLeft) {
         if (!world.isRemote) {
             LOGGER.info("Stopped using, {} left", timeLeft);
         }
-        else {
-            world.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, entity.posX, entity.posY + entity.getEyeHeight(), entity.posZ, 0, 0 ,0);
+        if (entity instanceof PlayerEntity) {
+            Optional<Spell> storedSpell = ScepterItem.getSpell(stack);
+            if (storedSpell.isPresent()) {
+                Spell spell = storedSpell.get();
+                int ticks = this.getUseDuration(stack) - timeLeft;
+                if (!world.isRemote) {
+                    LOGGER.info("Stopped casting {}", spell.getRegistryName());
+                }
+                spell.onCastComplete(entity.getEntityWorld(), new PlayerSpellcaster((PlayerEntity) entity), ticks);
+            }
+            ((PlayerEntity)entity).getCooldownTracker().setCooldown(stack.getItem(), 15);
         }
     }
 
     @Override
     @Nonnull
-    public ItemStack onItemUseFinish(@Nonnull ItemStack stack, World worldIn, @Nonnull LivingEntity entityLiving) {
-        if (!worldIn.isRemote) {
-            LOGGER.info("Item use finished");
+    public ItemStack onItemUseFinish(@Nonnull ItemStack stack, World world, @Nonnull LivingEntity entity) {
+        int timeLeft = 0;
+        if (!world.isRemote) {
+            LOGGER.info("Stopped using, {} left", timeLeft);
         }
-        else {
-            worldIn.addParticle(ParticleTypes.ANGRY_VILLAGER, entityLiving.posX, entityLiving.posY + entityLiving.getEyeHeight(), entityLiving.posZ, 0, 0,0);
+        if (entity instanceof PlayerEntity) {
+            Optional<Spell> storedSpell = ScepterItem.getSpell(stack);
+            if (storedSpell.isPresent()) {
+                Spell spell = storedSpell.get();
+                int ticks = this.getUseDuration(stack) - timeLeft;
+                if (!world.isRemote) {
+                    LOGGER.info("Completing the cast of {}", spell.getRegistryName());
+                }
+                spell.onCastComplete(entity.getEntityWorld(), new PlayerSpellcaster((PlayerEntity) entity), ticks);
+            }
+            ((PlayerEntity)entity).getCooldownTracker().setCooldown(stack.getItem(), 15);
         }
-        return super.onItemUseFinish(stack, worldIn, entityLiving);
+        return stack;
     }
 }
--- src/main/java/industries/immington/spellcraft/spell/Spell.java
+++ src/main/java/industries/immington/spellcraft/spell/Spell.java
@@ -1,6 +1,11 @@
 package industries.immington.spellcraft.spell;
 
+import industries.immington.spellcraft.Spellcraft;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistryEntry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Common (server and client) info about a spell.
@@ -8,23 +13,46 @@ import net.minecraftforge.registries.ForgeRegistryEntry;
  * You should subclass this in order to implement your spells.
  */
 public abstract class Spell extends ForgeRegistryEntry<Spell> {
+    protected final static Logger LOGGER = LogManager.getLogger(Spellcraft.MOD_ID);
     private SpellDomain domain;
     private SpellTargeting targeting;
     private int maxPotency;
     private int chargeTime;
 
+    public static DamageSource FADING_DAMAGE = new DamageSource("spellcraft:fading")
+            .setMagicDamage().setDamageBypassesArmor().setDamageIsAbsolute().setDifficultyScaled();
+    public static DamageSource SPELL_FIRE_DAMAGE = new DamageSource("spellcraft:fireSpell")
+            .setMagicDamage().setFireDamage();
+    public static DamageSource SPELL_DAMAGE = new DamageSource("spellcraft:spell")
+            .setMagicDamage();
+
     public SpellDomain getDomain() {
         return this.domain;
     }
 
-    public Spell(SpellDomain domain, SpellTargeting targeting, int chargeTime, int maxPotency) {
+    public Spell(String registryName, SpellDomain domain, SpellTargeting targeting, int chargeTime, int maxPotency) {
         this.domain = domain;
         this.targeting = targeting;
         this.chargeTime = chargeTime;
         this.maxPotency = maxPotency;
+        this.setRegistryName(registryName);
     }
 
     public int getMaxPotency() { return this.maxPotency; }
 
+    public int getChargeTime() { return this.chargeTime; }
+
     public SpellTargeting getTargeting() { return this.targeting; }
+
+    public boolean checkCastingBegin(World world, ISpellcaster caster) {
+        return true;
+    }
+
+    public boolean onWarmupTick(World world, ISpellcaster caster, int tick) {
+        return true;
+    }
+
+    public abstract boolean onCastTick(World world, ISpellcaster caster, int potency);
+
+    public abstract boolean onCastComplete(World world, ISpellcaster caster, int potency);
 }
