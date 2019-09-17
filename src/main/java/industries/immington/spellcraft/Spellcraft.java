dfc6d6e8d631aa488b355124794bfb50fa0b5d9d
S. B. Immington <snirk.immington@gmail.com>
2019-09-17 13:55:10 -0400

    Register spells, domains, entities

--- src/main/java/industries/immington/spellcraft/Spellcraft.java
+++ src/main/java/industries/immington/spellcraft/Spellcraft.java
@@ -1,9 +1,13 @@
 package industries.immington.spellcraft;
 
+import industries.immington.spellcraft.entity.Entities;
+import industries.immington.spellcraft.entity.FlungBlockEntity;
+import industries.immington.spellcraft.item.ScepterItem;
 import industries.immington.spellcraft.spell.Spell;
 import industries.immington.spellcraft.spell.SpellDomain;
-import industries.immington.spellcraft.item.ScepterItem;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
@@ -17,7 +21,6 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.RegistryBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -31,12 +34,7 @@ public class Spellcraft
     /** Spellcraft mod ID. */
     public static final String MOD_ID = "spellcraft";
 
-    public static class SpellDomains {
-    }
-
     public static class Registries {
-        public static IForgeRegistry<SpellDomain> domains;
-        public static IForgeRegistry<Spell> spells;
     }
 
     // public static CreativeTabs SPELLCRAFT_CREATIVE_TAB = null; // new SpellcraftCreativeTab();
@@ -91,35 +89,66 @@ public class Spellcraft
         @SubscribeEvent
         public static void registerItems(final RegistryEvent.Register<Item> event) {
             event.getRegistry().registerAll(
-                new ScepterItem(ItemTier.WOOD, 2, -2.2F, new Item.Properties().group(ItemGroup.TOOLS))
+                new ScepterItem(ItemTier.WOOD, 2, -2.2F, SpellDomain.LIFE, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:wooden_scepter"),
-                new ScepterItem(ItemTier.WOOD, 3, -2F, new Item.Properties().group(ItemGroup.TOOLS))
+                new ScepterItem(ItemTier.WOOD, 3, -2F, SpellDomain.UNDEAD, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:undead_scepter"),
-                new ScepterItem(ItemTier.WOOD, 3, -2.1F, new Item.Properties().group(ItemGroup.TOOLS))
+                new ScepterItem(ItemTier.WOOD, 3, -2.1F, SpellDomain.OCEAN, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:ocean_scepter"),
-                new ScepterItem(ItemTier.GOLD, 3, -2F, new Item.Properties().group(ItemGroup.TOOLS))
+                new ScepterItem(ItemTier.GOLD, 3, -2F, SpellDomain.ILLAGER, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:emerald_scepter"),
-                new ScepterItem(ItemTier.IRON, 4, -2F, new Item.Properties().group(ItemGroup.TOOLS))
+                /* Redstone */
+                new ScepterItem(ItemTier.IRON, 4, -2F, SpellDomain.NETHER, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:nether_scepter"),
-                new ScepterItem(ItemTier.GOLD, 3, -2.1F, new Item.Properties().group(ItemGroup.TOOLS))
+                new ScepterItem(ItemTier.GOLD, 3, -2.1F, SpellDomain.ENDER, new Item.Properties().group(ItemGroup.TOOLS))
                     .setRegistryName("spellcraft:ender_scepter")
             );
         }
 
         @SubscribeEvent
+        public static void registerEntities(final RegistryEvent.Register<EntityType<?>> event) {
+            Entities.FLUNG_BLOCK_ENTITY = EntityType.Builder.create(FlungBlockEntity::new, EntityClassification.MISC)
+                    .size(1F, 1F)
+                    .setShouldReceiveVelocityUpdates(true)
+                    .build("spellcraft:flung_block");
+            Entities.FLUNG_BLOCK_ENTITY.setRegistryName("spellcraft:flung_block");
+            event.getRegistry().register(Entities.FLUNG_BLOCK_ENTITY);
+        }
+
+        @SubscribeEvent
         public static void registerRegistries(final RegistryEvent.NewRegistry event) {
-            Registries.spells = new RegistryBuilder<Spell>()
+            Spells.registry = new RegistryBuilder<Spell>()
                     .allowModification()
                     .setDefaultKey(new ResourceLocation("spellcraft:empty"))
                     .setType(Spell.class)
                     .setName(new ResourceLocation("spellcraft:spells"))
                     .create();
-            Registries.domains = new RegistryBuilder<SpellDomain>()
+            SpellDomain.registry = new RegistryBuilder<SpellDomain>()
                     .allowModification()
                     .setDefaultKey(new ResourceLocation("spellcraft:empty"))
                     .setType(SpellDomain.class)
                     .setName(new ResourceLocation("spellcraft:domains"))
                     .create();
+        }
+
+        @SubscribeEvent
+        public static void registerSpellDomains(final RegistryEvent.Register<SpellDomain> event) {
+            event.getRegistry().registerAll(
+                    SpellDomain.LIFE,
+                    SpellDomain.UNDEAD,
+                    SpellDomain.ILLAGER,
+                    SpellDomain.REDSTONE,
+                    SpellDomain.OCEAN,
+                    SpellDomain.NETHER,
+                    SpellDomain.ENDER
+            );
+        }
+
+        @SubscribeEvent
+        public static void registerSpells(final RegistryEvent.Register<Spell> event) {
+            event.getRegistry().registerAll(
+                    Spells.FLING
+            );
         }
     }
 }
